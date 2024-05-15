9fdfb3b3da07b1de261c46fcbe534948069319c4
felix <felixffrs@gmail.com>
2024-05-15 20:47:46 -0300

    update: adding action decorator to test specific endpoint

--- author_crud/api/serializers/review_serializers.py
+++ author_crud/api/serializers/review_serializers.py
@@ -2,8 +2,24 @@ from rest_framework import serializers
 from author_crud.models.review import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    #book = serializers.StringRelatedField()
 
     class Meta:
         model = Review
         fields = ('id', 'rating', 'description', 'book')
+
+class ReviewBookSerializer(serializers.ModelSerializer):
+    book = serializers.StringRelatedField()
+
+    class Meta:
+        model = Review
+        fields = ('id', 'rating', 'description', 'book')
+
+class ChangeReviewRatingSerializer(serializers.Serializer):
+    rating = serializers.IntegerField()
+
+    def validate(self, data):
+        if data["rating"] > 10 or data["rating"] < 0:
+            raise serializers.ValidationError({
+                "rating": "Rating can't be more than 10 and less than 0"
+            })
+        return data
--- author_crud/api/views/review_views.py
+++ author_crud/api/views/review_views.py
@@ -1,25 +1,40 @@
 from rest_framework import viewsets, status, generics
 from rest_framework import status
 from rest_framework.response import Response
-from author_crud.api.serializers.book_serializers import BookSerializer
-from author_crud.api.serializers.review_serializers import ReviewSerializer
+from rest_framework.decorators import action
+from ...models.review import Review
+from author_crud.api.serializers.review_serializers import ReviewSerializer, ReviewBookSerializer, ChangeReviewRatingSerializer
 
 class BookReviewViewSet(viewsets.ModelViewSet):
+    model = Review
     serializer_class = ReviewSerializer
+    review_book_serializer_class = ReviewBookSerializer
+    change_review_rating_serializer_class = ChangeReviewRatingSerializer
 
-    def get_queryset(self, pk=None):
-        print(pk, "ddsadsdas")
-        if not pk:
+    def get_queryset(self, book_pk=None):
+        if not book_pk:
             return self.get_serializer().Meta.model.objects.all()
-        print(self.get_serializer().Meta.model.objects)
-        return self.get_serializer().Meta.model.objects.filter(book_id = pk)
+        return self.get_serializer().Meta.model.objects.filter(book_id = book_pk)
     
-    # def get_object(self, pk):
-    #     print(pk, "ewqeqwweqqew")
-    #     return generics.get_object_or_404(Book, pk=pk)
+    def get_object(self, book_pk=None):
+        pk = self.kwargs["pk"]
+        return generics.get_object_or_404(self.model, pk=pk)
+
+    @action(detail=True, methods=["PUT"], url_path="change-rating")
+    def change_rating(self, request, book_pk, pk):
+        serializer = self.change_review_rating_serializer_class(data=request.data)
+        review = self.get_object(pk)
+
+        if serializer.is_valid():
+            review.rating = serializer.data["rating"]
+            review.save()
+            return Response({
+                "message": f"Rating of review {pk} updated successfully"
+            })
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
     def list(self, request, book_pk) -> Response:
-        serializer = self.get_serializer(self.get_queryset(book_pk), many=True)
+        serializer = self.review_book_serializer_class(self.get_queryset(book_pk), many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
     
     def create(self, request, book_pk) -> Response:
@@ -29,11 +44,13 @@ class BookReviewViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
-    # def get(self, request, pk=None):
-    #     book = self.get_object(pk)
-    #     print(book, "dasdsasdaa")
-    #     if book:
-    #         serializer = BookSerializer(book)
-    #         return Response(serializer.data, status = status.HTTP_200_OK)
-    #     return Response({'message': f'Book {pk} not found'}, status = status.HTTP_400_BAD_REQUEST)
+    def retrieve(self, request, book_pk=None, pk=None):
+        print(book_pk, pk)
+        print(self.kwargs)
+        review = self.get_object(pk)
+        print(review)
+        if review:
+            serializer = self.serializer_class(review)
+            return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response({'message': f'Book {pk} not found'}, status = status.HTTP_400_BAD_REQUEST)
     
